da3b580dc7df5e76eaa866c562eb2d15a3714c10
Christopher Wu <cwu267@gmail.com>
2021-11-11 13:59:35 -0500

    moved api requests to inside app.js and passed them as props. Also made account balance dynamic

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Home from './Home';
 import LogIn from './Login';
 import UserProfile from './UserProfile';
 import Credit from './Credit';
+import Debit from './Debit';
 import './style.css';
-
+import axios from 'axios';
 
 class App extends Component {
 
@@ -17,7 +18,9 @@ class App extends Component {
       currentUser: {
         userName: 'John React',
         memberSince: '03/18/2016',
-      }
+      },
+      credits: [],
+      debits: []
     }
   }
 
@@ -26,12 +29,71 @@ class App extends Component {
     newUser.userName = logInInfo.userName
     this.setState({currentUser: newUser})
   }
+  async componentDidMount(){
+    let APIlink = "https://moj-api.herokuapp.com/credits";
+    try 
+    {
+      let response = await axios.get(APIlink);//await waits until we get data
+      this.setState({credits: response.data});
+      
+    } 
+    catch (error) 
+    {
+      if (error.response) 
+      {
+          /*
+           * The request was made and the server responded with a
+           * status code that falls out of the range of 2xx
+           */
+          console.log(error.response.data); //Not Found
+          console.log(error.response.status); //404
+          
+      }
+    }
+
+    let APIlink2 = "https://moj-api.herokuapp.com/debits";
+    try 
+    {
+      let response = await axios.get(APIlink2);//await waits until we get data
+      this.setState({debits: response.data});
+      
+    } 
+    catch (error) 
+    {
+      if (error.response) 
+      {
+          /*
+           * The request was made and the server responded with a
+           * status code that falls out of the range of 2xx
+           */
+          console.log(error.response.data); //Not Found
+          console.log(error.response.status); //404
+          
+      }
+    }
+    let credits = this.state.credits;
+    let debits = this.state.debits;
+    let balance = this.state.accountBalance;
+     credits.map((item) => 
+    {
+      balance += item.amount;
+
+      
+    })
+    debits.map((item) =>{
+      balance -= item.amount;
+
+    })
+    balance = Math.round(balance * 100) / 100;
+    this.setState({accountBalance : balance});
+    } 
 
   render() {
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
     const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
     const UserProfileComponent = () => (<UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}  />);
-    const CreditComponent = () => (<Credit accountBalance={this.state.accountBalance}/>);
+    const CreditComponent = () => (<Credit accountBalance={this.state.accountBalance} credits = {this.state.credits}/>);
+    const DebitComponent = () => (<Debit accountBalance={this.state.accountBalance} debits = {this.state.debits}/>);
   
 
     return (
@@ -41,6 +103,7 @@ class App extends Component {
           <Route exact path="/" render={HomeComponent}/>
           <Route exact path="/userProfile" render={UserProfileComponent}/>
           <Route exact path="/Credit" render={CreditComponent}/>
+          <Route exact path="/Debit" render={DebitComponent}/>
           </div>
         </Router>
     );
